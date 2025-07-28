5d610a0797022e103ebcbc154f2064cc0660ceae
Antoine Grimod <antoine.grimod@gmail.com>
2025-07-28 23:57:10 +0200

    [fix] double '\n' size

--- crabir/lib/post/widget/html_with_fade.dart
+++ crabir/lib/post/widget/html_with_fade.dart
@@ -25,20 +25,41 @@ class HtmlWithConditionalFade extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final htmlContent = this
+        .htmlContent
+        .replaceAll('<p>', '')
+        .replaceAll('</p>', '<br>')
+        .replaceAll('\n\n', '<br>');
     if (maxLines != null) {
       return Html(
-        data: htmlContent.replaceAll('<p>', '').replaceAll('</p>', '<br>'),
+        data: htmlContent,
         style: {
           '.md': Style(
             fontSize: FontSize(fontSize),
             maxLines: maxLines,
             textOverflow: TextOverflow.ellipsis,
           ),
+          "hr": Style(
+            margin: Margins.symmetric(vertical: 4), // reduce vertical space
+
+            padding: HtmlPaddings.zero,
+            height: Height(1), // control thickness
+            backgroundColor: Colors.grey, // ensure it's visible
+          ),
         },
       );
     } else {
       return Html(
         data: htmlContent,
+        style: {
+          "hr": Style(
+            margin: Margins.symmetric(vertical: 4), // reduce vertical space
+
+            padding: HtmlPaddings.zero,
+            height: Height(1), // control thickness
+            backgroundColor: Colors.grey, // ensure it's visible
+          ),
+        },
         onLinkTap: (url, _, __) async {
           final uri = Uri.parse(url!);
           if (await canLaunchUrl(uri)) {
