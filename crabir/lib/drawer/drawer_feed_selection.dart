3a7311591c09c945d3b43dbd995d44d64bee819d
Antoine Grimod <antoine.grimod@gmail.com>
2025-07-30 11:53:59 +0200

    remove BlocBuilder in favor of context.watch

--- crabir/lib/drawer/drawer_feed_selection.dart
+++ crabir/lib/drawer/drawer_feed_selection.dart
@@ -16,50 +16,48 @@ class DrawerFeedSelectionState extends State<DrawerFeedSelection> {
 
   @override
   Widget build(BuildContext context) {
-    final state = context.read<AccountsBloc>();
-    return BlocBuilder<AccountsBloc, AccountState>(
-      builder: (context, account) {
-        if (account.status == AccountStatus.uninit) {
-          state.add(Initialize());
-        } else if (account.status == AccountStatus.unloaded) {
-          state.add(LoadSubscriptions());
-        } else if (account.status == AccountStatus.loaded) {
-          return Flexible(
-            fit: FlexFit.loose,
-            child: ListView(
-              children: [
-                ...baseFeeds.map(
-                  (feed) => feed.toTile(context),
-                ),
-                Divider(),
-                ...userOptions.map((option) => ListTile(title: Text(option))),
-                Divider(),
-                ...account.multis.map((multi) => MultiRedditTile(
-                      multi: multi,
-                      closeDrawer: true,
-                    )),
-                Divider(),
-                ...account.subscriptions.map(
-                  (sub) => SubredditTile(
-                    sub: sub,
-                    closeDrawer: true,
-                  ),
-                ),
-                Divider(),
-                ListTile(
-                  onTap: () => showLicensePage(context: context),
-                  title: Text(
-                    "Licenses",
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  leading: Icon(Icons.info_outline),
-                ),
-              ],
+    final bloc = context.watch<AccountsBloc>();
+    final account = bloc.state;
+
+    if (account.status == AccountStatus.uninit) {
+      bloc.add(Initialize());
+    } else if (account.status == AccountStatus.unloaded) {
+      bloc.add(LoadSubscriptions());
+    } else if (account.status == AccountStatus.loaded) {
+      return Flexible(
+        fit: FlexFit.loose,
+        child: ListView(
+          children: [
+            ...baseFeeds.map(
+              (feed) => feed.toTile(context),
             ),
-          );
-        }
-        return Container();
-      },
-    );
+            Divider(),
+            ...userOptions.map((option) => ListTile(title: Text(option))),
+            Divider(),
+            ...account.multis.map((multi) => MultiRedditTile(
+                  multi: multi,
+                  closeDrawer: true,
+                )),
+            Divider(),
+            ...account.subscriptions.map(
+              (sub) => SubredditTile(
+                sub: sub,
+                closeDrawer: true,
+              ),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () => showLicensePage(context: context),
+              title: Text(
+                "Licenses",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              leading: Icon(Icons.info_outline),
+            ),
+          ],
+        ),
+      );
+    }
+    return Container();
   }
 }
