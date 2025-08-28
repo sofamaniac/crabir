7a1c98906bdcebbd8f3eadf2c397fe3404d8578b
Antoine Grimod <antoine.grimod@gmail.com>
2025-08-29 01:26:09 +0200

    better video/gif handling
    Thanks GPT-5 for this one

--- crabir/lib/media/gif.dart
+++ crabir/lib/media/gif.dart
@@ -1,62 +1,162 @@
 part of 'media.dart';
 
-class _GifContent extends StatefulWidget {
-  final VariantInner? mp4;
+class AnimatedContent extends StatefulWidget {
+  //final VariantInner? mp4;
 
   /// Used as a fallback if `mp4` is not available.
-  final VariantInner gif;
+  //final VariantInner gif;
   final String? placeholderUrl;
   final Resolution preferredResolution;
+  final int width;
+  final int height;
+  final String url;
 
-  const _GifContent(
-      {super.key,
-      required this.mp4,
-      required this.gif,
-      this.placeholderUrl,
-      this.preferredResolution = Resolution.source});
+  AnimatedContent.fromVariantInner({
+    super.key,
+    required VariantInner mp4,
+    this.placeholderUrl,
+    this.preferredResolution = Resolution.source,
+  })  : url = mp4.source.url,
+        width = mp4.source.width,
+        height = mp4.source.height;
+
+  AnimatedContent.fromMediaOEmbed({
+    super.key,
+    required Media_Oembed media,
+    this.placeholderUrl,
+    this.preferredResolution = Resolution.source,
+  })  : url = media.oembed.providerUrl,
+        width = media.oembed.width,
+        height = media.oembed.height;
+
+  AnimatedContent.fromRedditVideo({
+    super.key,
+    required Media_RedditVideo media,
+    this.placeholderUrl,
+    this.preferredResolution = Resolution.source,
+  })  : url = media.field0.dashUrl,
+        width = media.field0.width,
+        height = media.field0.height;
+
+  const AnimatedContent({
+    super.key,
+    required this.url,
+    required this.width,
+    required this.height,
+    this.placeholderUrl,
+    this.preferredResolution = Resolution.source,
+  });
   @override
-  State<_GifContent> createState() => _GifContentState();
+  State<AnimatedContent> createState() => _AnimatedContentState();
 }
 
-class _GifContentState extends State<_GifContent> {
-  late VideoPlayerController _controller;
-  late ChewieController _chewieController;
-  int width = 1;
-  int height = 1;
+class _AnimatedContentState extends State<AnimatedContent> {
+  late final VideoPlayerController _controller;
+  late final ChewieController _chewieController;
 
-  Widget placeholder() {
-    if (widget.placeholderUrl != null) {
-      return Image.network(widget.placeholderUrl!);
-    } else {
-      return Container();
-    }
-  }
+  /// Tracks which video should currently play and its score (distance / fraction)
+  static final ValueNotifier<String?> _currentlyPlayingUrl =
+      ValueNotifier(null);
+
+  /// Stores all registered videos
+  static final Map<String, _AnimatedContentState> _allVideos = {};
+
+  ScrollController? _scrollController;
 
   @override
   void initState() {
-    // TODO: only play when visible
-    // TODO: support resolution selection
     super.initState();
-    final source = widget.mp4?.source ?? widget.gif.source;
-    final url = source.url;
-    width = source.width;
-    height = source.height;
-    _controller = VideoPlayerController.networkUrl(Uri.parse(url));
+
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.url));
+    _controller.initialize().then((_) {
+      setState(() {});
+      _updateGlobalPlayState();
+    });
+
     _chewieController = ChewieController(
       videoPlayerController: _controller,
-      autoPlay: true,
+      autoPlay: false, // manual control
       looping: true,
-      aspectRatio: width / height,
+      aspectRatio: widget.width / widget.height,
       placeholder: placeholder(),
     );
+
+    // Register this video
+    _allVideos[widget.url] = this;
+
+    // Attach scroll listener
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController = Scrollable.of(context)?.widget.controller;
+      _scrollController ??= PrimaryScrollController.of(context);
+      _scrollController?.addListener(_onScroll);
+      _onScroll(); // initial check
+    });
+
+    _currentlyPlayingUrl.addListener(_updateGlobalPlayState);
+  }
+
+  Widget placeholder() => widget.placeholderUrl != null
+      ? Image.network(widget.placeholderUrl!)
+      : const SizedBox.shrink();
+
+  void _updateGlobalPlayState() {
+    if (_currentlyPlayingUrl.value == widget.url) {
+      if (!_controller.value.isPlaying) _controller.play();
+    } else {
+      if (_controller.value.isPlaying) _controller.pause();
+    }
+  }
+
+  void _onScroll() {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final bottomBarHeight = kBottomNavigationBarHeight;
+    final screenCenter = (screenHeight - bottomBarHeight) / 2;
+
+    String? bestUrl;
+    double bestScore = double.infinity;
+
+    for (final entry in _allVideos.entries) {
+      final videoState = entry.value;
+      final renderObject = videoState.context.findRenderObject();
+      if (renderObject is! RenderBox || !renderObject.hasSize) continue;
+
+      final size = renderObject.size;
+      final topLeftGlobal = renderObject.localToGlobal(Offset.zero);
+
+      // Compute visible fraction
+      final visibleTop = topLeftGlobal.dy.clamp(0, screenHeight);
+      final visibleBottom =
+          (topLeftGlobal.dy + size.height).clamp(0, screenHeight);
+      final visibleHeight = visibleBottom - visibleTop;
+      final visibleFraction = visibleHeight / size.height;
+
+      if (visibleFraction < 0.1) continue; // skip mostly hidden videos
+
+      // Compute distance from screen center
+      final videoCenter = topLeftGlobal.dy + size.height / 2;
+      final distance = (videoCenter - screenCenter).abs();
+
+      // score = distance / fraction
+      final score = distance / visibleFraction;
+
+      if (score < bestScore) {
+        bestScore = score;
+        bestUrl = entry.key;
+      }
+    }
+
+    if (_currentlyPlayingUrl.value != bestUrl) {
+      _currentlyPlayingUrl.value = bestUrl;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: AspectRatio(
-        aspectRatio: width / height,
+        aspectRatio: widget.width / widget.height,
         child: Chewie(
+          key: ValueKey(widget.url),
           controller: _chewieController,
         ),
       ),
@@ -65,6 +165,10 @@ class _GifContentState extends State<_GifContent> {
 
   @override
   void dispose() {
+    _currentlyPlayingUrl.removeListener(_updateGlobalPlayState);
+    _scrollController?.removeListener(_onScroll);
+    _allVideos.remove(widget.url);
+
     _controller.dispose();
     _chewieController.dispose();
     super.dispose();
--- crabir/lib/post/widget/video.dart
+++ crabir/lib/post/widget/video.dart
@@ -1,10 +1,8 @@
-import 'package:chewie/chewie.dart';
 import 'package:crabir/cartouche.dart';
 import 'package:crabir/media/media.dart';
 import 'package:crabir/src/rust/third_party/reddit_api/model/post.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
-import 'package:video_player/video_player.dart';
 
 class YoutubeContent extends StatelessWidget {
   final Post post;
@@ -30,75 +28,24 @@ class YoutubeContent extends StatelessWidget {
   }
 }
 
-class VideoContent extends StatefulWidget {
+class VideoContent extends StatelessWidget {
   final Post post;
   const VideoContent({super.key, required this.post});
-
-  @override
-  State<VideoContent> createState() => _VideoContentState();
-}
-
-class _VideoContentState extends State<VideoContent> {
-  late VideoPlayerController _controller;
-  late ChewieController _chewieController;
-  late int width;
-  late int height;
-
-  @override
-  void initState() {
-    super.initState();
-    final String url;
-    switch (widget.post.secureMedia) {
-      case null:
-        if (widget.post.preview?.images[0].variants.mp4 != null) {
-          url = widget.post.preview!.images[0].variants.mp4!.source.url;
-          width = widget.post.preview!.images[0].variants.mp4!.source.width;
-          height = widget.post.preview!.images[0].variants.mp4!.source.height;
-        } else {
-          url = "";
-          width = 1;
-          height = 1;
-        }
-        break;
-      case Media_RedditVideo():
-        final video = (widget.post.secureMedia! as Media_RedditVideo);
-        url = video.field0.fallbackUrl;
-        width = video.field0.width.toInt();
-        height = video.field0.height.toInt();
-        break;
-      case Media_Oembed():
-        final video = (widget.post.secureMedia! as Media_Oembed);
-        url = video.oembed.providerUrl;
-        width = video.oembed.width.toInt();
-        height = video.oembed.height.toInt();
-        break;
-    }
-    _controller = VideoPlayerController.networkUrl(Uri.parse(url));
-    //_controller.initialize();
-    _chewieController = ChewieController(
-      videoPlayerController: _controller,
-      autoPlay: true,
-      looping: true,
-      aspectRatio: width / height,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AspectRatio(
-        aspectRatio: width / height,
-        child: Chewie(
-          controller: _chewieController,
+    final media = post.secureMedia;
+    return switch (media) {
+      Media_RedditVideo() => AnimatedContent.fromRedditVideo(media: media),
+      Media_Oembed() => AnimatedContent.fromMediaOEmbed(media: media),
+      null when post.preview?.images[0].variants.mp4 != null =>
+        AnimatedContent.fromVariantInner(
+          mp4: post.preview!.images[0].variants.mp4!,
         ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    _chewieController.dispose();
-    super.dispose();
+      null when post.preview?.images[0].variants.gif != null =>
+        AnimatedContent.fromVariantInner(
+          mp4: post.preview!.images[0].variants.gif!,
+        ),
+      _ => Center(child: Text("Error while loading video")),
+    };
   }
 }
