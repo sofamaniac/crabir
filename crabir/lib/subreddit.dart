e359c285cbc65752b9ae8875ab633f214f1fed89
Antoine Grimod <antoine.grimod@gmail.com>
2025-09-10 00:39:20 +0200

    fix subreddit icon

--- crabir/lib/subreddit.dart
+++ crabir/lib/subreddit.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:crabir/hexcolor.dart';
 import 'package:crabir/src/rust/third_party/reddit_api/model/subreddit.dart'
     as subreddit;
@@ -14,11 +15,18 @@ class SubredditIcon extends StatelessWidget {
     return switch (icon) {
       subreddit.SubredditIcon_Image(field0: final icon) => CircleAvatar(
           radius: radius,
-          foregroundImage: NetworkImage(icon.url),
           backgroundColor: Colors.transparent,
+          child: ClipOval(
+            child: Image(
+              image: CachedNetworkImageProvider(icon.url),
+              width: radius * 2,
+              height: radius * 2,
+              fit: BoxFit.contain,
+            ),
+          ),
         ),
       subreddit.SubredditIcon_Color(field0: final color) => CircleAvatar(
-          radius: 16,
+          radius: radius,
           backgroundColor: color.stringToColor(),
           child: Text("r/"),
         )
