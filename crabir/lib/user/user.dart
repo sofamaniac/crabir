38846726818aa397fa0ebc5e3558d374918843e8
Antoine Grimod <antoine.grimod@gmail.com>
2025-09-22 11:21:01 +0200

    fix: user view

--- crabir/lib/user/user.dart
+++ crabir/lib/user/user.dart
@@ -1,9 +1,10 @@
+import 'dart:math' as math;
+
 import 'package:auto_route/auto_route.dart';
 import 'package:crabir/accounts/bloc/accounts_bloc.dart';
 import 'package:crabir/loading_indicator.dart';
 import 'package:crabir/post/post.dart';
 import 'package:crabir/routes/routes.dart';
-import 'package:crabir/settings/theme/theme.dart';
 import 'package:crabir/src/rust/api/reddit_api.dart';
 import 'package:crabir/src/rust/third_party/reddit_api/model/comment.dart';
 import 'package:crabir/src/rust/third_party/reddit_api/model/post.dart';
@@ -47,6 +48,7 @@ class _UserView extends StatefulWidget {
 
 class _UserViewState extends State<_UserView> {
   late Future<UserInfo> _userFuture;
+  double opacity = 0;
 
   @override
   void initState() {
@@ -92,7 +94,7 @@ class _UserViewState extends State<_UserView> {
               return SafeArea(
                 child: ExtendedNestedScrollView(
                   onlyOneScrollInBody: true,
-                  floatHeaderSlivers: true,
+                  floatHeaderSlivers: false,
                   headerSliverBuilder: (
                     BuildContext context,
                     bool innerBoxIsScrolled,
@@ -116,35 +118,57 @@ class _UserViewState extends State<_UserView> {
 
   List<Widget> topBar(
       UserInfo infos, TabController tabController, List<UserTabs> tabs) {
-    return <Widget>[
-      SliverAppBar.large(
-        flexibleSpace: FlexibleSpaceBar(
-          background: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // TODO: add banner
-              CircleAvatar(
-                radius: 40,
-                backgroundImage: NetworkImage(infos.iconImg),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                infos.name,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-        ),
-        pinned: false,
-        floating: false,
+    final title = Text(
+      infos.name,
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
       ),
-      SliverPersistentHeader(
+    );
+    return <Widget>[
+      SliverAppBar(
+        expandedHeight: 200,
         pinned: true,
-        delegate: _TabBarDelegate(
-          TabBar(
+        floating: false,
+        title: Opacity(opacity: 1 - opacity, child: title),
+        flexibleSpace: LayoutBuilder(
+          builder: (context, constraints) {
+            final settings = context.dependOnInheritedWidgetOfExactType<
+                FlexibleSpaceBarSettings>()!;
+            final deltaExtent = settings.maxExtent - settings.minExtent;
+            final t = (1.0 -
+                    (settings.currentExtent - settings.minExtent) / deltaExtent)
+                .clamp(0.0, 1.0);
+            final fadeStart = math.max(0.0, 1.0 - kToolbarHeight / deltaExtent);
+            const fadeEnd = 1.0;
+            WidgetsBinding.instance.addPostFrameCallback(
+              (_) => setState(() {
+                opacity = 1.0 - Interval(fadeStart, fadeEnd).transform(t);
+              }),
+            );
+
+            return Opacity(
+              opacity: opacity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // TODO: add banner
+                  Flexible(
+                      child: CircleAvatar(
+                    radius: 40,
+                    backgroundImage: NetworkImage(infos.iconImg),
+                  )),
+                  const SizedBox(height: 8),
+                  Flexible(child: title),
+                ],
+              ),
+            );
+          },
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(16),
+          child: TabBar(
             isScrollable: true,
             tabAlignment: TabAlignment.start,
             tabs: tabs.map((tab) => Text(tab.name())).toList(),
@@ -154,34 +178,4 @@ class _UserViewState extends State<_UserView> {
       ),
     ];
   }
-}
-
-/// Custom delegate to pin the TabBar
-class _TabBarDelegate extends SliverPersistentHeaderDelegate {
-  final TabBar tabBar;
-  _TabBarDelegate(this.tabBar);
-
-  @override
-  double get minExtent => tabBar.preferredSize.height;
-  @override
-  double get maxExtent => tabBar.preferredSize.height;
-
-  @override
-  Widget build(
-    BuildContext context,
-    double shrinkOffset,
-    bool overlapsContent,
-  ) {
-    final theme = CrabirTheme.of(context);
-    return SizedBox(
-      height: tabBar.preferredSize.height,
-      child: ColoredBox(
-        color: theme.background,
-        child: tabBar,
-      ),
-    );
-  }
-
-  @override
-  bool shouldRebuild(_TabBarDelegate oldDelegate) => false;
 }
