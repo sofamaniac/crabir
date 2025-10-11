a58ddd12db5d2e4f1c80607b685b387a44328189
Antoine Grimod <antoine.grimod@gmail.com>
2025-10-11 23:52:19 +0200

    go to home tab when switching account

--- crabir/lib/accounts/widgets/account_selector.dart
+++ crabir/lib/accounts/widgets/account_selector.dart
@@ -3,6 +3,7 @@ import 'package:crabir/accounts/bloc/accounts_bloc.dart';
 import 'package:crabir/accounts/widgets/login_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class AccountSelector extends StatelessWidget {
   /// When set to false, do not display a button for the current account.
@@ -31,6 +32,7 @@ class AccountSelector extends StatelessWidget {
                   bloc.add(SelectAccount(index));
                   onTapCallback?.call();
                   Scaffold.of(context).closeDrawer();
+                  context.go("/");
                 },
                 leading: CircleAvatar(
                   radius: 16,
