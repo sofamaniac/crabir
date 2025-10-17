ade28ceac03c5d576df7caef721f8cacc14fc44a
Antoine Grimod <antoine.grimod@gmail.com>
2025-10-17 12:26:39 +0200

    signing apk

--- crabir/android/app/build.gradle.kts
+++ crabir/android/app/build.gradle.kts
@@ -42,21 +42,33 @@ android {
         versionName = flutter.versionName
     }
 
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+    if (keystorePropertiesFile.exists()) {
+        signingConfigs {
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-            signingConfig = signingConfigs.getByName("release")
+            if (keystorePropertiesFile.exists()) {
+              signingConfig = signingConfigs.getByName("release")
+            } else {
+              signingConfig = signingConfigs.getByName("debug")
+              println("⚠️ keystore.properties not found, using debug signing config for release builds.")
+            }
+        }
+        debug {
+            if (keystorePropertiesFile.exists()) {
+              signingConfig = signingConfigs.getByName("release")
+            } else {
+              signingConfig = signingConfigs.getByName("debug")
+              println("⚠️ keystore.properties not found, using debug signing config for release builds.")
+            }
         }
     }
 }
