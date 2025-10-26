e725f2cb8e434ee14fb41bfb0297292b400ae567
Antoine Grimod <antoine.grimod@gmail.com>
2025-10-26 10:23:06 +0100

    cleanup logs

--- crabir/lib/subscription/widgets/paywall.dart
+++ crabir/lib/subscription/widgets/paywall.dart
@@ -2,20 +2,22 @@ import 'package:crabir/routes/routes.dart';
 import 'package:crabir/subscription/bloc/subscription_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:purchases_ui_flutter/purchases_ui_flutter.dart';
 
 class PaywallScreen extends StatelessWidget {
   const PaywallScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: presentPaywallIfNeeded(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          context.go(DEFAULT_ROUTE);
-        }
-        return SizedBox.shrink();
-      },
-    );
+    // return FutureBuilder(
+    //   future: presentPaywallIfNeeded(),
+    //   builder: (context, snapshot) {
+    //     if (snapshot.connectionState == ConnectionState.done) {
+    //       context.go(DEFAULT_ROUTE);
+    //     }
+    //     return SizedBox.shrink();
+    //   },
+    // );
+    return PaywallView();
   }
 }
--- crabir_bridge/src/api/mod.rs
+++ crabir_bridge/src/api/mod.rs
@@ -3,5 +3,29 @@ pub mod reddit_api;
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
     // Default utilities - feel free to customize
-    flutter_rust_bridge::setup_default_user_utils();
+    // flutter_rust_bridge::setup_default_user_utils();
+    setup_log_to_console();
+    std::env::set_var("RUST_BACKTRACE", "1");
+}
+
+fn setup_log_to_console() {
+    let level = log::LevelFilter::Trace;
+    let filter = env_filter::Builder::new()
+        .filter_module("h2", log::LevelFilter::Error)
+        .build();
+    #[cfg(target_os = "android")]
+    let _ = android_logger::init_once(
+        android_logger::Config::default()
+            .with_max_level(level)
+            .with_filter(filter),
+    );
+
+    #[cfg(any(target_os = "ios", target_os = "macos"))]
+    let _ = oslog::OsLogger::new("frb_user")
+        .level_filter(level)
+        .with_filter(filter)
+        .init();
+
+    #[cfg(target_family = "wasm")]
+    let _ = crate::misc::web_utils::WebConsoleLogger::init();
 }
