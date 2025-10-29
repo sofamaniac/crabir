46618c4f2afeb3c1bd55b878302230660b7bb18e
Antoine Grimod <antoine.grimod@gmail.com>
2025-10-29 12:23:37 +0100

    fix rust logging

--- crabir_bridge/src/api/mod.rs
+++ crabir_bridge/src/api/mod.rs
@@ -1,30 +1,25 @@
+use log::LevelFilter;
+
 pub mod reddit_api;
 
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
     // Default utilities - feel free to customize
-    // flutter_rust_bridge::setup_default_user_utils();
+    //flutter_rust_bridge::setup_default_user_utils();
     setup_log_to_console();
-    std::env::set_var("RUST_BACKTRACE", "1");
+    //std::env::set_var("RUST_BACKTRACE", "1");
 }
 
 fn setup_log_to_console() {
-    let level = log::LevelFilter::Trace;
-    let filter = env_filter::Builder::new()
-        //.filter_module("h2", log::LevelFilter::Error)
-        .build();
+    let level = LevelFilter::Debug;
     #[cfg(target_os = "android")]
-    let _ = android_logger::init_once(
-        android_logger::Config::default()
-            .with_max_level(level)
-            .with_filter(filter),
-    );
+    {
+        let config = android_logger::Config::default().with_max_level(level);
+        let _ = android_logger::init_once(config);
+    }
 
     #[cfg(any(target_os = "ios", target_os = "macos"))]
-    let _ = oslog::OsLogger::new("frb_user")
-        .level_filter(level)
-        .with_filter(filter)
-        .init();
+    let _ = oslog::OsLogger::new("frb_user").level_filter(level).init();
 
     #[cfg(target_family = "wasm")]
     let _ = crate::misc::web_utils::WebConsoleLogger::init();
