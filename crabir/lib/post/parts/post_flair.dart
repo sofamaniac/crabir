e08c2daa611b704b90d990a1353b57d00462e87d
Antoine Grimod <antoine.grimod@gmail.com>
2025-10-31 12:10:10 +0100

    fix: navigate to search on flair tap

--- crabir/lib/post/parts/post_flair.dart
+++ crabir/lib/post/parts/post_flair.dart
@@ -11,16 +11,10 @@ class PostFlair extends StatelessWidget {
     final Widget flairWidget;
     if (settings.tapFlairToSearch) {
       flairWidget = InkWell(
-        // onTap: () => context.router.root.navigate(
-        //   SearchPostsRoute(
-        //     // provide key to differentiate the pages
-        //     // but prevents rebuilding when clicking the same flair again
-        //     key: ValueKey("${post.subreddit.subreddit}-${post.linkFlair}"),
-        //     subreddit: post.subreddit.subreddit,
-        //     flair: post.linkFlair,
-        //   ),
-        // ),
-        onTap: () => context.go("/search-posts"),
+        onTap: () => SearchPostsView(
+          subreddit: post.subreddit.subreddit,
+          flair: post.linkFlair,
+        ).goNamed(context),
         child: FlairView(
           flair: post.linkFlair,
           showColor: settings.showFlairColors,
