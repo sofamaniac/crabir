6626abb7ac94466b730005b8ae5ce8fc66ba23c6
Antoine Grimod <antoine.grimod@gmail.com>
2026-02-18 17:47:02 +0100

    Markdown now supports superscript (but not markdown inside superscript)

---
+++ app/src/main/java/com/sofamaniac/reboost/ui/markdown/PassThroughTextView.kt
@@ -0,0 +1,54 @@
+package com.sofamaniac.reboost.ui.markdown
+
+import android.content.Context
+import android.text.Spanned
+import android.text.style.ClickableSpan
+import android.text.style.URLSpan
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatTextView
+
+class PassThroughTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = android.R.attr.textViewStyle
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event == null) {
+            return super.onTouchEvent(event)
+        }
+
+        val action = event.action
+        val layout = layout ?: return super.onTouchEvent(event)
+        val x = event.x.toInt() - totalPaddingLeft
+        val y = event.y.toInt() - totalPaddingTop
+        val line = layout.getLineForVertical(y)
+        val offset = layout.getOffsetForHorizontal(line, x.toFloat())
+
+        val clickableSpans = listOf(URLSpan::class.java, SpoilerSpan::class.java)
+        var clickedSpan: ClickableSpan? = null
+        for (span in clickableSpans) {
+            val spans = (text as? Spanned)?.getSpans(offset, offset, span)
+            if (!spans.isNullOrEmpty()) {
+                // Stop on the first clickable span found
+                clickedSpan = spans.first()
+                break
+            }
+        }
+
+        if (clickedSpan == null) {
+            return false
+        }
+        when (action) {
+            MotionEvent.ACTION_DOWN -> return true
+            MotionEvent.ACTION_UP -> {
+                performClick()
+                clickedSpan.onClick(this)
+                return true
+            }
+        }
+        // Pass all other events to parent
+        return false
+    }
+}
---
+++ app/src/main/java/com/sofamaniac/reboost/ui/markdown/RedditMarkdown.kt
@@ -0,0 +1,252 @@
+/*
+ * Copyright (c) 2025 Antoine Grimod
+ */
+
+package com.sofamaniac.reboost.ui.markdown
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.text.Layout
+import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.view.ViewTreeObserver
+import android.widget.TextView
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.net.toUri
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.request.target.Target
+import com.sofamaniac.reboost.data.remote.dto.post.MediaMetadata
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.noties.markwon.AbstractMarkwonPlugin
+import io.noties.markwon.Markwon
+import io.noties.markwon.core.MarkwonTheme
+import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
+import io.noties.markwon.ext.tables.TablePlugin
+import io.noties.markwon.html.HtmlPlugin
+import io.noties.markwon.image.AsyncDrawable
+import io.noties.markwon.image.glide.GlideImagesPlugin
+import io.noties.markwon.inlineparser.MarkwonInlineParserPlugin
+import jakarta.inject.Inject
+
+
+/** Display Reddit style markdown.
+ * @param markdown The markdown to display.
+ * @param modifier The modifier to apply to the text.
+ * @param maxLines The maximum number of lines to display.
+ *
+ * If [maxLines] is different from [Int.MAX_VALUE], the link in the text will not be clickable
+ */
+@Composable
+fun SimpleMarkdown(
+    markdown: String,
+    modifier: Modifier = Modifier,
+    maxLines: Int = Int.MAX_VALUE,
+    mediaMetadata: Map<String, MediaMetadata> = emptyMap(),
+    key: String? = markdown,
+    viewModel: MarkdownViewModel = hiltViewModel(key = key)
+) {
+    val colorScheme = MaterialTheme.colorScheme
+
+
+    val processedMarkdown = markdown
+        .convertRedditSpoilers()
+        .convertRedditPreviewLinks()
+        .convertRedditSuperscript()
+
+
+    val context = LocalContext.current
+    val textView = remember { PassThroughTextView(context) }
+
+    // Fix missing ellipsis https://github.com/noties/Markwon/issues/180
+    textView.getViewTreeObserver()
+        .addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                val max = textView.maxLines
+                val layout: Layout? = textView.layout
+                if ((layout?.lineCount ?: 0) > max) {
+                    val end = layout!!.getLineEnd(max - 1);
+                    textView.setText(
+                        textView.getText().subSequence(0, end - 3),
+                        TextView.BufferType.SPANNABLE
+                    );
+                    textView.append("...");
+                }
+            }
+        });
+
+    val markwonReddit = remember(redditMarkwonBuilder(context, colorScheme, mediaMetadata))
+    // Parse markdown once and remember it
+    val parsedMarkdown = remember(processedMarkdown, markwonReddit) {
+        markwonReddit.parse(processedMarkdown)
+    }
+
+    val spanned = remember(parsedMarkdown, markwonReddit) {
+        markwonReddit.render(parsedMarkdown)
+    }
+
+    AndroidView(
+//        onReset = {
+//            markwonReddit.setParsedMarkdown(textView, spanned)
+//            textView.tag = markdown
+//        },
+        factory = { context ->
+            textView.apply {
+                setTextColor(colorScheme.onBackground.toArgb())
+                setLinkTextColor(colorScheme.primary.toArgb())
+                //this.movementMethod = LinkTouchMovementMethod.getInstance()
+                this.maxLines = maxLines
+                markwonReddit.setParsedMarkdown(textView, spanned)
+
+                // Disable link when truncating view and allow clicks to be passed to parent view.
+                if (maxLines != Int.MAX_VALUE) {
+                    textView.movementMethod = null
+                    textView.ellipsize = TextUtils.TruncateAt.END
+                } else {
+                    textView.movementMethod = LinkMovementMethod.getInstance()
+                }
+                textView.tag = markdown
+            }
+        },
+        modifier = modifier
+            .onSizeChanged { size ->
+                viewModel.height = size.height
+            }
+            .let { mod ->
+                if (maxLines != Int.MAX_VALUE) {
+                    return@let mod
+                }
+                viewModel.height?.let { size ->
+                    mod.then(Modifier.height(with(LocalDensity.current) { size.toDp() }))
+                } ?: mod
+            },
+        update = { textView ->
+            if (textView.tag != markdown) {
+                textView.tag = markdown
+                markwonReddit.setParsedMarkdown(textView, spanned)
+                // Disable link when truncating view and allow clicks to be passed to parent view.
+                if (maxLines != Int.MAX_VALUE) {
+                    textView.movementMethod = null
+                } else {
+                    textView.movementMethod = LinkMovementMethod.getInstance()
+                }
+            }
+        }
+    )
+}
+
+@Composable
+private fun redditMarkwonBuilder(
+    context: Context,
+    colorScheme: ColorScheme,
+    mediaMetadata: Map<String, MediaMetadata>
+): () -> Markwon = {
+    Markwon.builder(context)
+        .usePlugin(MarkwonInlineParserPlugin.create())
+        .usePlugin(StrikethroughPlugin.create())
+        .useRedditSpoilers()
+        .usePlugin(HtmlPlugin.create())
+        .usePlugin(TablePlugin.create(context))
+        .usePlugin(MarkdownTheme(colorScheme))
+        .usePlugin(HtmlPlugin.create())
+        .usePlugin(
+            GlideImagesPlugin.create(
+                object : GlideImagesPlugin.GlideStore {
+                    override fun load(drawable: AsyncDrawable): RequestBuilder<Drawable?> {
+                        val metadata = drawable.getMetadata(mediaMetadata)
+                        val placeholder =
+                            Color.GRAY.toDrawable()
+                        placeholder.setBounds(0, 0, metadata?.width ?: 0, metadata?.height ?: 0)
+                        return Glide.with(context)
+                            .load(metadata?.toMediaResource()?.url ?: drawable.destination)
+                            .placeholder(
+                                placeholder
+                            )
+                    }
+
+                    override fun cancel(target: Target<*>) {
+                        Glide.with(context).clear(target)
+                    }
+                }
+
+            ))
+        //.usePlugin(FullWidthImagePlugin(mediaMetadata))
+        .build()
+}
+
+private fun AsyncDrawable.getMetadata(mediaMetadata: Map<String, MediaMetadata>): MediaMetadata? {
+    // Handle destination of the form `giphy|xxxx`
+    if (destination.contains('|')) {
+        return mediaMetadata[destination]
+    }
+    // Otherwise assume destination is a link
+    val url = destination.toUri()
+    val filename = url.pathSegments.last().split('.').first()
+    return mediaMetadata[filename]
+}
+
+private fun String.convertRedditSpoilers(): String {
+    return this.replace(">!", " \ue000 ").replace("!<", " \ue000 ")
+}
+
+private fun String.convertRedditPreviewLinks(): String {
+    // Match Reddit preview links that aren't already in markdown syntax
+    val redditPreviewPattern = Regex(
+        """(?<!]\()https://preview\.redd\.it/[^\s)]+(?!\))"""
+    )
+
+    return redditPreviewPattern.replace(this) { matchResult ->
+        //"<img width=\"100%\" src=\"${matchResult.value}\"/>"
+        "![](${matchResult.value})"
+    }
+}
+
+private fun String.convertRedditSuperscript(): String {
+    // Match Reddit preview links that aren't already in markdown syntax
+    val redditSuperscriptPattern = Regex(
+        """\^\(([^)]+)\)|\^\^(\S+)"""
+    )
+
+    return redditSuperscriptPattern.replace(this) { matchResult ->
+        "<sup>${
+            matchResult.groupValues[1].ifEmpty { matchResult.groupValues[2] }
+        }</sup > "
+    }
+}
+
+@HiltViewModel
+class MarkdownViewModel @Inject constructor() : ViewModel() {
+    var height: Int? = null
+
+    init {
+        height = null
+    }
+}
+
+class MarkdownTheme(val colorScheme: ColorScheme) : AbstractMarkwonPlugin() {
+    override fun configureTheme(builder: MarkwonTheme.Builder) {
+        builder
+            .linkColor(colorScheme.primary.toArgb())
+            .codeTextColor(colorScheme.onBackground.toArgb())
+            .codeBackgroundColor(colorScheme.background.toArgb())
+            .codeBlockBackgroundColor(colorScheme.background.toArgb())
+            .blockQuoteColor(colorScheme.primary.toArgb())
+            // Disable ruler under titles ?
+            .headingBreakColor(colorScheme.background.toArgb())
+    }
+}
