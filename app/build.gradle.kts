2b9d468ea72304837efa7536db5926dd15111930
Antoine Grimod <antoine.grimod@gmail.com>
2026-03-17 00:44:37 +0100

    bump: version code & name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.sofamaniac.crabir"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.3"
+        versionCode = 11
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
