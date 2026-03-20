652554cd42e7b36436e5f4c74aa92a1d5b697bbb
Antoine Grimod <antoine.grimod@gmail.com>
2026-03-20 09:35:04 +0100

    fix: Add text when there is no comment to display

--- app/src/main/java/com/sofamaniac/crabir/ui/thread/CommentListRoot.kt
+++ app/src/main/java/com/sofamaniac/crabir/ui/thread/CommentListRoot.kt
@@ -1,19 +1,28 @@
 package com.sofamaniac.crabir.ui.thread
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Comment
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.sofamaniac.crabir.LocalTheme
 import com.sofamaniac.crabir.domain.model.CommentType
 
@@ -37,11 +46,31 @@ fun CommentListRoot(
         val comments by viewModel.comments
             .collectAsState(initial = emptyList())
         val post by viewModel.post.collectAsState(initial = null)
-        LazyColumn(modifier = Modifier.fillMaxSize(), state = listState) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            state = listState,
+            verticalArrangement = Arrangement.spacedBy(2.dp)
+        ) {
             // Show post
             item {
                 if (post != null) {
                     PostView(post!!, threadViewModel = viewModel)
+                }
+            }
+            if (comments.isEmpty() && !isRefreshing) {
+                item {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Icon(Icons.AutoMirrored.Filled.Comment, contentDescription = null)
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            "No comments",
+                        )
+                    }
                 }
             }
             items(comments.size, key = { index -> comments[index].name }) { index ->
@@ -63,9 +92,6 @@ fun CommentListRoot(
                             .background(color = theme.cardBackground)
                             .depthIndent(comment.depth, color = Color.Gray)
                     )
-                }
-                if ((comments.getOrNull(index + 1)?.depth ?: 0) == 0) {
-                    HorizontalDivider()
                 }
             }
             item {
