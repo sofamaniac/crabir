92f9da338a001124732039002f380ad5c6a620b8
Antoine Grimod <antoine.grimod@gmail.com>
2026-05-05 14:10:42 +0200

    fix: avoid crash when no preview is available for a gallery element

--- app/src/main/java/com/sofamaniac/crabir/ui/post/PostGallery.kt
+++ app/src/main/java/com/sofamaniac/crabir/ui/post/PostGallery.kt
@@ -133,7 +133,7 @@ fun EmbeddedGallery(
                     }
 
                     is MediaMetadata.Image -> {
-                        metadata.preview!!.last().url
+                        metadata.preview?.last()?.url
                     }
 
                     else -> null
