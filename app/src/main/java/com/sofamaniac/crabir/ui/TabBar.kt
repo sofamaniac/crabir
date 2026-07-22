a93e887a7539c0b46e995d458e0b96252679c239
Antoine Grimod <antoine.grimod@gmail.com>
2026-07-22 13:46:04 +0200

    feat: badge showing the number of unread messages in bottom tab bar

--- app/src/main/java/com/sofamaniac/crabir/ui/TabBar.kt
+++ app/src/main/java/com/sofamaniac/crabir/ui/TabBar.kt
@@ -2,20 +2,24 @@ package com.sofamaniac.crabir.ui
 
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.List
-import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Mail
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import com.sofamaniac.crabir.LocalRedditAccount
+import com.sofamaniac.crabir.LocalTheme
 import com.sofamaniac.crabir.R
 import com.sofamaniac.crabir.navigation.HomeRoute
 import com.sofamaniac.crabir.navigation.InboxRoute
@@ -24,7 +28,6 @@ import com.sofamaniac.crabir.navigation.ProfileRoute
 import com.sofamaniac.crabir.navigation.Route
 import com.sofamaniac.crabir.navigation.SearchRoute
 import com.sofamaniac.crabir.navigation.SubscriptionsRoute
-import com.sofamaniac.crabir.ui.user.ProfileTabs
 
 internal class TabRepresentation(val icon: ImageVector, val label: Int, val route: Route)
 
@@ -36,47 +39,94 @@ fun TabBar(
     onTabReselect: (() -> Unit)? = null,
 ) {
     val user = LocalRedditAccount.current
-    val tabs = listOf(
-        TabRepresentation(Icons.Filled.Home, R.string.Home, HomeRoute),
-        TabRepresentation(Icons.Default.Search, R.string.Search, SearchRoute()),
-        TabRepresentation(
-            Icons.AutoMirrored.Outlined.List, R.string.Subscriptions,
-            SubscriptionsRoute
-        ),
-        TabRepresentation(Icons.Default.Email, R.string.Inbox, InboxRoute),
-        TabRepresentation(
-            Icons.Filled.Person,
-            R.string.Profile,
-            ProfileRoute(user.info?.username ?: "Anonymous", ProfileTabs.Overview)
-        )
-    )
-    val navController = LocalNavController.current!!
+    val navController = LocalNavController.current
+    val theme = LocalTheme.current
+    fun onClick(selected: Int, index: Int, destination: Route) {
+        if (onTabReselect != null && selected == index) {
+            onTabReselect()
+        } else {
+            navController?.navigate(destination) {
+                popUpTo(navController.graph.startDestinationId) {
+                    saveState = index == 0
+                }
+                launchSingleTop = true
+            }
+        }
+    }
     PrimaryTabRow(
         selectedTabIndex = selected,
         modifier = modifier.navigationBarsPadding(),
-        indicator = {}) {
-        for ((index, tab) in tabs.withIndex()) {
-            Tab(
-                selected = selected == index,
-                unselectedContentColor = Color.Gray,
-                onClick = {
-                    if (onTabReselect != null && selected == index) {
-                        return@Tab onTabReselect()
-                    }
-                    if (index == 4 && user.isAnonymous()) {
-                        // TODO: ask user to log in
-                        return@Tab
-                    }
-                    navController.navigate(tab.route) {
-                        popUpTo(navController.graph.startDestinationId) {
-                            saveState = index == 0
+        indicator = {}
+    ) {
+        Tab(
+            selected = selected == 0,
+            unselectedContentColor = Color.Gray,
+            onClick = { onClick(selected, 0, HomeRoute) },
+            icon = {
+                Icon(
+                    Icons.Filled.Home,
+                    contentDescription = stringResource(R.string.Home)
+                )
+            }
+        )
+        Tab(
+            selected = selected == 1,
+            unselectedContentColor = Color.Gray,
+            onClick = { onClick(selected, 1, SearchRoute()) },
+            icon = {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = stringResource(R.string.Search)
+                )
+            }
+        )
+        Tab(
+            selected = selected == 2,
+            unselectedContentColor = Color.Gray,
+            onClick = { onClick(selected, 2, SubscriptionsRoute) },
+            icon = {
+                Icon(
+                    Icons.AutoMirrored.Filled.List,
+                    contentDescription = stringResource(R.string.Subscriptions)
+                )
+            }
+        )
+        Tab(
+            selected = selected == 3,
+            unselectedContentColor = Color.Gray,
+            onClick = { onClick(selected, 3, InboxRoute) },
+            icon = {
+                BadgedBox(badge = {
+                    if ((user.info?.inboxCount ?: 0) > 0) {
+                        Badge(containerColor = theme.highlight, contentColor = theme.contentColor) {
+                            Text("${user.info?.inboxCount}")
                         }
-                        launchSingleTop = true
                     }
-                },
-                icon = { Icon(tab.icon, contentDescription = stringResource(tab.label)) },
-            )
-        }
+                }) {
+                    Icon(
+                        Icons.Filled.Mail,
+                        contentDescription = stringResource(R.string.Inbox)
+                    )
+                }
+            }
+        )
+        Tab(
+            selected = selected == 4,
+            unselectedContentColor = Color.Gray,
+            onClick = {
+                if (!user.isAnonymous() && !user.isUninitialized()) {
+                    onClick(selected, 4, ProfileRoute(user.info?.username ?: "Anonymous"))
+                } else {
+                    // TODO ask user to log in
+                }
+            },
+            icon = {
+                Icon(
+                    Icons.Filled.Person,
+                    contentDescription = stringResource(R.string.Profile)
+                )
+            }
+        )
     }
 }
 
